b43e2c0db4511da05ce9a1524719d22a5273dc64
davii66 <61777918+davii66@users.noreply.github.com>
2026-02-18 10:39:00 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_PD1986-user
-add_lunch_combo omni_PD1986-userdebug
-add_lunch_combo omni_PD1986-eng
+#add_lunch_combo omni_PD1986-user
+#add_lunch_combo omni_PD1986-userdebug
+#add_lunch_combo omni_PD1986-eng
